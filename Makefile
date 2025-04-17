1fb0351dddece24de466411121fac426cbd9d668
fluteur <malo.leroy@student-cs.fr>
2025-04-18 00:25:57 +0200

    ⬆️ Migrate to latest version of Confer

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ OBJ_FILES = obj/fQX.o obj/fZX.o obj/print.o obj/rational.o
 TARGETS =  $(OBJ_FILES)
 
 CC_OPTIONS = -pedantic
-CC_INCLUDE = -I./src
+CC_INCLUDE = -I./include
 WARN = -Wall -Wextra
 CFLAGS = $(CC_INCLUDE) $(CC_OPTIONS) $(WARN)
 obj/cpn.o: $(OBJ_FILES)
@@ -15,11 +15,13 @@ obj/cpn.o: $(OBJ_FILES)
 	@ld -r $(OBJ_FILES) -o $@
 
 obj/%.o: src/%.c
+	@mkdir -p obj
 	@echo "🔨 $< -> $@"
-	@$(CC) -I./src -c $< -o $@ $(CFLAGS)
+	@$(CC) -I./include -c $< -o $@ $(CFLAGS)
 
 ./bin/test: test/test.c $(OBJ_FILES)
-	@$(CC) "test/test.c" $(CONFER_PATH)/obj/confer.o $(OBJ_FILES) $(CFLAGS) -I./test -I$(CONFER_PATH)/src -o "./bin/test"
+	@mkdir -p bin
+	@$(CC) "test/test.c" $(CONFER_PATH)/obj/confer.o $(OBJ_FILES) $(CFLAGS) -I./test -I$(CONFER_PATH)/include -o "./bin/test"
 
 clean:
 	rm ${TARGETS}
--- test/test_rational.c
+++ test/test_rational.c
@@ -8,19 +8,19 @@ void test_Q(CFTEST);
 void test_operations(CFTEST);
 
 void test_rational(CFTEST) {
-    cfTest(test_integers);
-    cfTest(test_Q);
-    cfTest(test_operations);
+    cf_test(test_integers);
+    cf_test(test_Q);
+    cf_test(test_operations);
 }
 
 void test_integers(CFTEST) {
     // abs
-    assertIntEqual(abs(3), 3);
-    assertIntEqual(abs(-3), 3);
+    assert_int_equal(abs(3), 3);
+    assert_int_equal(abs(-3), 3);
 
     // gcd
-    assertIntEqual(gcd(15, 7), 1);
-    assertIntEqual(gcd(12, 18), 6);
+    assert_int_equal(gcd(15, 7), 1);
+    assert_int_equal(gcd(12, 18), 6);
 }
 
 void test_Q(CFTEST) {
@@ -28,20 +28,20 @@ void test_Q(CFTEST) {
     Q two_thirds = new_Q(2, 3);
 
     // is_zero_Q
-    assertTrue(is_zero_Q(new_Q(0, 1)));
-    assertFalse(is_zero_Q(one_half));
+    assert_true(is_zero_Q(new_Q(0, 1)));
+    assert_false(is_zero_Q(one_half));
 
     // zero_Q
-    assertTrue(is_zero_Q(zero_Q()));
+    assert_true(is_zero_Q(zero_Q()));
 
     // are_equal_Q
-    assertTrue(are_equal_Q(one_half, new_Q(8, 16)));
-    assertFalse(are_equal_Q(one_half, two_thirds));
+    assert_true(are_equal_Q(one_half, new_Q(8, 16)));
+    assert_false(are_equal_Q(one_half, two_thirds));
 
     // is_int
-    assertTrue(is_int_Q(new_Q(6, 3)));
-    assertFalse(is_int_Q(one_half));
-    assertTrue(is_int_Q(zero_Q()));
+    assert_true(is_int_Q(new_Q(6, 3)));
+    assert_false(is_int_Q(one_half));
+    assert_true(is_int_Q(zero_Q()));
 }
 
 void test_operations(CFTEST) {
@@ -51,9 +51,9 @@ void test_operations(CFTEST) {
     Q a_minus_b = new_Q(-93, 20);
 
     // * and /
-    // assertTrue(are_equal_Q());
+    // assert_true(are_equal_Q());
 
     // + and -
-    assertTrue(are_equal_Q(add_Q(a, b), a_plus_b));
-    assertTrue(are_equal_Q(substract_Q(a, b), a_minus_b));
+    assert_true(are_equal_Q(add_Q(a, b), a_plus_b));
+    assert_true(are_equal_Q(substract_Q(a, b), a_minus_b));
 }
